72f03607ea8a62fbeac3725bf3bd1628c6b36d0a
apple <apple@apples-macbook-pro.local>
2016-06-30 16:33:07 +0800

    练习

---
+++ LianXI/LianXI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LianXI
+//
+//  Created by apple on 16/6/30.
+//  Copyright © 2016年 XiaoYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LianXI/LianXI/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LianXI
+//
+//  Created by apple on 16/6/30.
+//  Copyright © 2016年 XiaoYu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"yang 开始练习");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
